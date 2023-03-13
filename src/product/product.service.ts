151a6519794a08d5e75d5a7b53b25920e7ca4870
fnanoia <francisconanoia@hotmail.com>
2023-03-13 20:53:52 -0300

    product service & controller

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,26 +1,107 @@
-import { Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './schema/product.schema';
 
 @Injectable()
 export class ProductService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(@InjectModel(Product.name) private productModel: Model<any>) {}
+
+  async create(createProductDto: CreateProductDto) {
+    try {
+      const product = await this.productModel.create(createProductDto);
+      return { product, message: 'Product created successfully' };
+    } catch (error: any) {
+      throw new HttpException(
+        {
+          status: HttpStatus.BAD_REQUEST,
+          error: 'Error creating product',
+        },
+        HttpStatus.BAD_REQUEST,
+        { cause: error },
+      );
+    }
   }
 
-  findAll() {
-    return `This action returns all product`;
+  async findAll() {
+    try {
+      const products = await this.productModel.find();
+      return products;
+    } catch (error: any) {
+      throw new HttpException(
+        {
+          status: HttpStatus.NOT_FOUND,
+          error: 'Products not found',
+        },
+        HttpStatus.NOT_FOUND,
+        { cause: error },
+      );
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string) {
+    try {
+      const product = await this.productModel.findOne({ _id: id });
+      return product;
+    } catch (error: any) {
+      throw new HttpException(
+        {
+          status: HttpStatus.NOT_FOUND,
+          error: 'Product not found',
+        },
+        HttpStatus.NOT_FOUND,
+        { cause: error },
+      );
+    }
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async updateOne(id: string, updateProductDto: UpdateProductDto) {
+    try {
+      const checkProduct = await this.findOne(id);
+      if (!checkProduct) {
+        throw new NotFoundException({ message: 'Product not found' });
+      }
+
+      const product = await this.productModel.findOneAndUpdate(
+        { _id: id },
+        updateProductDto,
+        { new: true },
+      );
+
+      return { product, message: 'Product updated successfully' };
+    } catch (error: any) {
+      throw new HttpException(
+        {
+          status: HttpStatus.BAD_REQUEST,
+          error: 'Error updating product',
+        },
+        HttpStatus.BAD_REQUEST,
+        { cause: error },
+      );
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async deleteOne(id: string) {
+    try {
+      const product = await this.productModel.findOneAndDelete({ _id: id });
+
+      return { product, message: 'Product deleted successfully' };
+    } catch (error: any) {
+      throw new HttpException(
+        {
+          status: HttpStatus.BAD_REQUEST,
+          error: 'Error deleting product',
+        },
+        HttpStatus.BAD_REQUEST,
+        { cause: error },
+      );
+    }
   }
 }
