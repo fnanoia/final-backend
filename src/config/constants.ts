2b9632df99b14e109b9bdcc7c02d285c85160414
fnanoia <francisconanoia@hotmail.com>
2023-03-24 21:38:51 -0300

    env fix

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,11 +2,11 @@ export const NODE_ENV = process.env.NODE_ENV;
 
 export const PORT = process.env.PORT;
 
-export const DB_URI = process.env.DB_URI;
-export const DB_URI_PROD = process.env.DB_URI_PROD;
-export const DB_NAME = process.env.DB_NAME;
+export const DB_URI = 'DB_URI';
+export const DB_URI_PROD = 'DB_URI_PROD';
+export const DB_NAME = 'DB_NAME';
 
-export const JWT_SECRET = process.env.JWT_SECRET;
+export const JWT_SECRET = 'JWT_SECRET';
 
 export const ADMIN_EMAIL = process.env.ADMIN_EMAIL;
 export const ADMIN_PWD = process.env.ADMIN_PWD;
